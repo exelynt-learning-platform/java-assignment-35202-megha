e44e91000be4549546153706ab4cf093e48cb356
MeghaShinde000 <meghashindecm87@gmail.com>
2026-07-31 22:16:50 +0530

    Code added to branch

---
+++ MyClass.java
@@ -0,0 +1,22 @@
+   // This is the MyClass program
+    public class MyClass
+    {
+        //This method prints a welcome msg
+        public static void custom1()
+        {
+            System.out.print("Welcome to java programmin!");
+        }
+        //This method prints a learning msg
+        public static void custom2()
+        {
+            System.out.print("I am learning java programming!");
+        }
+        //This is main methos , execution starts here.
+        public static void main(String args[])
+        {
+            //Calling of first method
+            custom1();
+            //calling of second method
+            custom2();
+        }
+    }

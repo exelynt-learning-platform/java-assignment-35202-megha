24ea03eb9b370318fac26615cebc3c4a4a658c6a
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-03 14:11:55 +0530

    Added StudentInfo and Calculator programs

---
+++ StudentInfo.java
@@ -0,0 +1,13 @@
+//Display basic student information
+public class StudentInfo
+{
+     // Main method
+    public static void main(String args[])
+    {
+        String studentName = "Megha";
+        int studentAge = 21;
+// Display of information
+        System.out.println("Student Name:"+studentName);
+        System.out.println("Student Age:"+studentAge);
+    }
+}

0d708d6f8cd79d5f1e528848579d1dc3a3b59618
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-03 14:10:44 +0530

    fix

--- MyClass.java
+++ MyClass.java
@@ -1,22 +1,23 @@
-   // This is the MyClass program
-    public class MyClass
-    {
-        //This method prints a welcome msg
-        public static void custom1()
-        {
-            System.out.print("Welcome to java programmin!");
-        }
-        //This method prints a learning msg
-        public static void custom2()
-        {
-            System.out.print("I am learning java programming!");
-        }
-        //This is main methos , execution starts here.
-        public static void main(String args[])
-        {
-            //Calling of first method
-            custom1();
-            //calling of second method
-            custom2();
-        }
+// This is the MyClass program
+public class MyClass {
+
+    // This method prints a welcome message
+    public static void custom1() {
+        System.out.println("Welcome to Java Programming!");
     }
+
+    // This method prints a learning message
+    public static void custom2() {
+        System.out.println("I am learning Java programming!");
+    }
+
+    // Main method - execution starts here
+    public static void main(String[] args) {
+
+        // Calling the first method
+        custom1();
+
+        // Calling the second method
+        custom2();
+    }
+}

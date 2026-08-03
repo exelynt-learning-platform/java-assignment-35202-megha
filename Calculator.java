abb47e7ecdc00887c66f5248c2cf89bb412b6034
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-03 14:20:40 +0530

    Renamed Calculator file and fixed class name

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
- public class Calculater
+ public class Calculator
  {
-  public static int add(int num1,int num2)
+  public static int add(int num1 , int num2)
   {
       return num1+num2;
 
   }
   public static void main(String args[])
   {
-      int result=add(10,20);
+      int result=add(10 , 20);
       System.out.println(result);
   }
 }

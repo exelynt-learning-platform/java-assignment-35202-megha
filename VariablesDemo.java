e63de1db38b2b523f60ba19a2e67e3b042835165
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-05 18:28:15 +0530

    Added VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,30 @@
+public class VariablesDemo
+    {
+        public static void main(String args[])
+        {
+            // Int displays the integer value
+            int a=5;
+            // Float displays the floating numbers
+            float b=4.5f;
+            // String displays the  string of words
+            String c="Megha";
+            // short displays the short value
+            short d=100;
+            // long displays th long value
+            long e=1000000;
+            // char stores the character
+            char f='A';
+            // boolean displays the true false value
+            boolean g=true;
+            // double stores the long decimal no
+            double h=1234.5678;
+            System.out.println("Integer value:"+a);
+            System.out.println("float value:"+b);
+            System.out.println("string value:"+c);
+            System.out.println("short value:"+d);
+            System.out.println("long value:"+e);
+            System.out.println("char value:"+f);
+            System.out.println("boolean value:"+g);
+            System.out.println("double value:"+h);
+         }
+    }

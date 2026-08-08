d0f053799efc2630e08554f30834113b901d4e51
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-08 10:54:51 +0530

    Added file to github

---
+++ OperatorsDemo.java
@@ -0,0 +1,41 @@
+public class OperatorsDemo
+{
+    public static void main(String args[])
+    {
+        // Assignment operator assigns the value to the variable.
+        int a=10,b=2;
+        // Arithmetic operators used for  basic mathematical operations
+        int add=a+b;//Addition
+        int sub=a-b;//Subtraction
+        int mul=a*b;//Multiplication
+        int div=a/b;//Division
+        int mod=a%b;//Modulos
+        //Display the arithmetic operations results
+        System.out.println("Assignment operator:"+a);
+        System.out.println("Addition operation:"+add);
+        System.out.println("subtraction operation:"+div);
+        System.out.println("multiplication operation:"+mul);
+        System.out.println("Division operation:"+div);
+        //Relational operators to compare values
+         boolean Greater=a > b;// Greator
+         boolean Smaller = a<b;//Smaller
+        //Print relational operators result
+        System.out.println("A is Greater"+Greater);
+        System.out.println("A is Smaller"+Smaller);
+        //Logical operators
+        boolean  AND = (a>0)&&(b>0);
+        boolean OR = (a>0)||(b<0);
+        //Result of logical operators
+        System.out.println("Logical and"+AND);
+        System.out.println("Logical or"+OR);
+        // Bitwise Operation
+        int bitwiseAnd = a & b; // Bitwise AND
+
+        // Print bitwise result
+
+        System.out.println("Bitwise AND (a & b): " + bitwiseAnd);
+
+
+
+    }
+}

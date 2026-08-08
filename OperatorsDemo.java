4583a4b473946f519ea5a1afb3700405599c638f
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-08 10:59:38 +0530

    Modified  file to github

--- OperatorsDemo.java
+++ OperatorsDemo.java
@@ -13,18 +13,18 @@ public class OperatorsDemo
         //Display the arithmetic operations results
         System.out.println("Assignment operator:"+a);
         System.out.println("Addition operation:"+add);
-        System.out.println("subtraction operation:"+div);
+        System.out.println("subtraction operation:"+sub);
         System.out.println("multiplication operation:"+mul);
         System.out.println("Division operation:"+div);
         //Relational operators to compare values
          boolean Greater=a > b;// Greator
-         boolean Smaller = a<b;//Smaller
+         boolean Smaller = a < b;//Smaller
         //Print relational operators result
         System.out.println("A is Greater"+Greater);
         System.out.println("A is Smaller"+Smaller);
         //Logical operators
-        boolean  AND = (a>0)&&(b>0);
-        boolean OR = (a>0)||(b<0);
+        boolean  AND = (a>0) && (b>0);
+        boolean OR = (a>0) || (b<0);
         //Result of logical operators
         System.out.println("Logical and"+AND);
         System.out.println("Logical or"+OR);

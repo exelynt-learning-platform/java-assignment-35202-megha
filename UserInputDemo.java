da4d338019328783039b0dbe8dc7a42fd9f21723
MeghaShinde000 <meghashindecm87@gmail.com>
2026-08-08 19:16:58 +0530

    Removed unnacessory files added to github

--- UserInputDemo.java
+++ UserInputDemo.java
@@ -6,25 +6,25 @@ import java.util.Scanner;
 {
             public static void main(String[] args) {
 
-                Scanner sc = new Scanner(System.in);
+                Scanner scanner = new Scanner(System.in);
 
                 // Read user's name
                 System.out.print("Enter your name: ");
-                String name = sc.nextLine();
+                String name = scanner.nextLine();
 
                 // Read user's age
                 System.out.print("Enter your age: ");
-                int age = sc.nextInt();
+                int age = scanner.nextInt();
 
                 // Read user's favorite number
                 System.out.print("Enter your favorite number: ");
-                int favoriteNumber = sc.nextInt();
+                int favoriteNumber = scanner.nextInt();
 
                 // Display personalized message
                 System.out.println("Hello " + name );
                 System.out.println("Your age is  " + age );
                 System.out.println("Your favorite number is " + favoriteNumber );
 
-                sc.close();
+                scanner.close();
             }
         }
